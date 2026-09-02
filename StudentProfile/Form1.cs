9b669ed8dd167bbd1dd834d087f02279166c27cf
Kharl Hendrich Soriano <kharlsoriano37@gmail.com>
2026-09-02 12:26:50 +0800

    Add StudentProfile Visual Studio Project

---
+++ StudentProfile/Form1.cs
@@ -0,0 +1,15 @@
+namespace StudentProfile
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            MessageBox.Show("Student Profile" , "Kharl Soriano", MessageBoxButtons.OK, MessageBoxIcon.Information); 
+        }
+    }
+}
